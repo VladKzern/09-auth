6f5b5b8b062f896ab64de161b6873e3cf8e02c2b
Vladyslav Korniienko <vladyslav.korniienko@stud.onu.edu.ua>
2025-09-21 16:25:11 +0300

    add api to api.ts

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -2,6 +2,6 @@ import axios, { AxiosError } from "axios";
 
 export type ApiError = AxiosError<{ error: string }>;
 export const nextServer = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL + '/api',
   withCredentials: true,
 });
